cc3ceeb5ab2003cac71e31568658104bbc5fcd46
Andrei Grozavu <81321423+eazyistired@users.noreply.github.com>
2022-05-19 17:33:00 +0300

    Add files via upload

---
+++ levels/tile_sets/grass_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="grass_tiles" tilewidth="64" tileheight="64" tilecount="5" columns="5">
+ <image source="C:/Users/kwahe/Desktop/2 - Level/graphics/decoration/grass/grass.png" width="320" height="64"/>
+</tileset>
